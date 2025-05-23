ab4224292cd11a82bb3591b74fefcb738120c303
Hongdan Wang <hongdanw@umich.edu>
2025-05-23 17:39:38 +0800

    1

--- arkpalette/eslint.config.mjs
+++ arkpalette/eslint.config.mjs
@@ -13,7 +13,8 @@ const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
     rules: {
-      "@typescript-eslint/no-unused-vars": "off"
+      "@typescript-eslint/no-unused-vars": "off",
+      "prefer-const": "off"
     }
   }
 ];
